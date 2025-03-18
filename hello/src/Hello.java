06d68201e7737f9fcac57676a3ef32a8cc4680c4
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-03-18 19:12:36 +0100

    add Anton Hello

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -2,4 +2,9 @@ public class Hello {
     void helloIlia() {
         System.out.println("Ilia says Hello!!!");
     }
+
+    void helloAnton(){
+        System.out.println("Hello from Anton");
+
+    }
 }
